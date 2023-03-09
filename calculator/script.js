0c3173ac4c6877a852ae40d439a0f5dbf7cde726
nanovolt <127000482+nanovolt@users.noreply.github.com>
2023-03-09 17:12:57 +0600

    add input to calculator

--- calculator/script.js
+++ calculator/script.js
@@ -11,8 +11,31 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator) {
-    switch(operator) {
+const topOutput = document.querySelector(".top");
+topOutput.textContent = "";
+const input = document.querySelector(".input");
+input.textContent = "";
+
+const allclear = document.querySelector(".allclear");
+const clear = document.querySelector(".clear");
+const numbers = document.querySelectorAll(".number");
+const operations = document.querySelectorAll(".operation");
+const dot = document.querySelector(".dot");
+const equals = document.querySelector(".equals");
+
+let lastEntry;
+let topValue = "";
+let pressedOperation = false;
+let enableDecimal = true;
+
+function showOnTop(str) {
+    topOutput.textContent = str;
+}
+function updateLastEntry () {
+    lastEntry = input.textContent[input.textContent.length - 1];
+}
+function calculate(a, operation, b) {
+    switch(operation) {
         case "+" :
             add(a, b);
             break;
@@ -27,23 +50,90 @@ function operate(operator) {
             break;
     }
 }
+function parseString(str) {
 
-const output = document.querySelector(".output");
-output.textContent = "";
+    let signs = /\+|\-|\*|\//;
 
-const allclear = document.querySelector(".allclear");
-allclear.onclick = function () {
-    output.textContent = "";
+    match = signs.exec(str);
+    if (match !== null) {
+        console.log("match:", match.index);
+        console.log("string:", str);
+        console.log(input.textContent.split(/\+|\-|\*|\//));
+    }
+    // console.log(+input.textContent);
+
+    // console.log(input.textContent.split(/\+|\-|\*|\//));
+
+    // let pattern = /\+|\-|\*|\//g;
+    // let signs = /\+|\-|\*|\//;
+    // let arr = [];
+    // let leftOperand = [];
+    // let rightOperand = [];
+    // let restOfTheString = [];
+    // let addRest = false;
+    
+    // match = pattern.exec(input.textContent);
+
+    // if (match !== null) {
+    //     console.log("match:", match.index);
+    //     console.log("string:", input.textContent);
+    //     for (let i = match.index-1; i >= 0; i--) {
+    //         leftOperand.unshift(input.textContent[i]);
+    //     }
+    //     for (let i = match.index+1; i < input.textContent.length; i++) {
+    //         if (i < input.textContent.length) {
+    //             rightOperand.unshift(input.textContent[i]);
+    //         }
+    //     }
+    //     let lo = leftOperand.join("");
+    //     arr.push({"operand":lo, "operation": input.textContent[match.index]});
+    //     console.log("arr:", arr);
+    // }
 }
-const numbers = document.querySelectorAll(".number");
-for (let number of numbers) {
-    number.addEventListener("click", ()=> {
-        if (number.textContent == ".") {
-            let lastInput = output.textContent[output.textContent.length - 1];
-            console.log("lastInput:", lastInput);
-            // output.textContent += "0.";  
+allclear.onclick = function() {
+    input.textContent = "";
+    topOutput.textContent = "";
+   
+}
+clear.onclick = function() {
+    input.textContent = input.textContent.slice(0, -1);
+}
+
+dot.onclick = function() {
+    if (enableDecimal) {
+        console.log("lastEntry:", lastEntry);
+        console.log("typeof lastEntry:", typeof +lastEntry);
+        if(true) {
+
         }
-        output.textContent += number.textContent;
-        console.log(output.textContent);
+        
+        enableDecimal = false;
+    }
+}
+equals.onclick = function() {
+    
+    topOutput.textContent = input.textContent + "=";
+    updateLastEntry();
+    showOnTop(topOutput.textContent);
+    input.textContent = parseString(topOutput.textContent);
+}
+for (let number of numbers) {
+    number.addEventListener("click", ()=> {   
+        input.textContent += number.textContent; 
+        updateLastEntry();
+    })
+}
+
+for (let operation of operations) {
+    operation.addEventListener("click", ()=> {
+        if (!pressedOperation && !operation.classList.contains("noshow")) {
+            input.textContent += operation.textContent;
+            pressedOperation = true;
+            enableDecimal = true;
+        }
+        if (pressedOperation) {
+
+        }
+        updateLastEntry();
     })
 }

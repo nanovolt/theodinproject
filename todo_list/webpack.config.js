5ef31b660ddab254bcaabcd5ac33b97cb000b211
nanovolt <127000482+nanovolt@users.noreply.github.com>
2023-04-08 15:01:10 +0600

    init todo_list project

---
+++ todo_list/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+  },
+  devServer: {
+    static: path.resolve(__dirname),
+    port: 9000,
+    client: {
+      logging: "none",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};

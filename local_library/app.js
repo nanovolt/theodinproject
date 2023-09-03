20a9af5a45cc02fa87ca5cfd68ec64b6aa30232e
nanovolt <127000482+nanovolt@users.noreply.github.com>
2023-09-03 21:27:58 +0600

    add models and controllers

--- local_library/app.js
+++ local_library/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -6,8 +7,11 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
 
 const app = express();
+
+console.log(`MONGO_URL:`, process.env.MONGO_URL);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- local_library/routes/index.js
+++ local_library/routes/index.js
@@ -5,7 +5,8 @@ const router = express.Router();
 /* GET home page. */
 // eslint-disable-next-line no-unused-vars
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  // res.render("index", { title: "Local library" });
+  res.redirect("/catalog");
 });
 
 module.exports = router;

f49906d1c614e55209d517c5db5a424f2d74cdd5
nanovolt <127000482+nanovolt@users.noreply.github.com>
2023-10-06 00:29:32 +0600

    add redis for session store

--- blog_api/backend/config/session.ts
+++ blog_api/backend/config/session.ts
@@ -3,7 +3,9 @@
 // require("dotenv").config();
 // const session = require("express-session");
 import session from "express-session";
-import MongoStore from "connect-mongo";
+// import MongoStore from "connect-mongo";
+import RedisStore from "connect-redis";
+import { redisClient } from "./redis";
 
 // const dbOptions = {
 //   useNewUrlParser: true,
@@ -14,10 +16,14 @@ const expressSession = session({
   secret: process.env.EXPRESS_SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGO,
-    collectionName: "sessions",
-    autoRemoveInterval: 60,
+  // store: MongoStore.create({
+  //   mongoUrl: process.env.MONGO,
+  //   collectionName: "sessions",
+  //   autoRemoveInterval: 60,
+  // }),
+  store: new RedisStore({
+    client: redisClient,
+    prefix: "blog_api:",
   }),
   cookie: {
     maxAge: 1000 * 60,

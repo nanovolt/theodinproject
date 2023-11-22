cb3a8435f10201963fc7492237f588906df1d0ec
nanovolt <127000482+nanovolt@users.noreply.github.com>
2023-11-23 01:38:52 +0600

    add dashboard for blog cms

--- blog_api/frontend_cms/src/pages/Home.tsx
+++ blog_api/frontend_cms/src/pages/Home.tsx
@@ -1,11 +1,25 @@
+import styles from "./Home.module.css";
+
 import { useTitle } from "../hooks/useTitle";
+import { currentUserApiSlice } from "../features/currentUser/currentUserSlice";
+import { postsApiSlice } from "../features/posts/postsApiSlice";
+import { categoriesApiSlice } from "../features/categories/categoriesApiSlice";
 
 export function Home() {
   useTitle("Home | Blog CMS");
 
+  const { data: currentUser } = currentUserApiSlice.useMeQuery();
+
+  const { data: posts } = postsApiSlice.useGetPostsQuery();
+
+  const { data: categories } = categoriesApiSlice.useGetCategoriesQuery();
+
   return (
-    <div>
-      <h1>Blog CMS</h1>
+    <div className={styles.dashboard}>
+      <h1>Dashboard</h1>
+      <p>User: {currentUser?.user.username}</p>
+      <p>Total posts: {posts?.length}</p>
+      <p>Total categories: {categories?.length}</p>
     </div>
   );
 }
